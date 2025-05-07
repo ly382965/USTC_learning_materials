aa848ff494a692fdebf393d47e54495b298c3cd0
mmd_ustc <134723501+ly382965@users.noreply.github.com>
2025-05-07 10:12:39 +0800

    mmd

--- 2SP Principles of Computers and Embedded Systems/实验/计嵌lab5/HARDWARE/exti/exti.h
+++
@@ -1,5 +0,0 @@
-#include "stm32f4xx_exti.h"
-#include "stm32f4xx_syscfg.h"
-
-void EXTIX_Init(void);
-
--- 2SP Principles of Computers and Embedded Systems/实验/计嵌综合实验/HARDWARE/exti/exti.h
+++
@@ -1,5 +0,0 @@
-#include "stm32f4xx_exti.h"
-#include "stm32f4xx_syscfg.h"
-
-void EXTI0_Init(void);
-void EXTI1_Init(void);
